e777d4ad05d87913c47ba660582fb4b5f55fba84
Edilberto Mejia Flores <edilberto.mejia@laureate.pe>
2024-05-24 13:45:42 -0500

    avance

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,19 @@
 FROM node:12.12.0-alpine
+
+# Establecer el directorio de trabajo en /appnodejs
 WORKDIR /appnodejs
-ADD . /appnodejs
+
+# Copiar el archivo package.json y package-lock.json (si existe) al directorio de trabajo
+COPY package*.json ./
+
+# Instalar dependencias
 RUN npm install
+
+# Copiar el resto del código de la aplicación
+COPY . .
+
+# Exponer el puerto 3000
 EXPOSE 3000
-CMD npm start
+
+# Definir el comando de inicio
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var app = express();
 
+// Ruta principal
 app.get('/', function (req, res) {
-  res.send('Hello World! Hola Mundo!');
+  // Obtener las variables de entorno
+  var keycloakUrl = process.env.KEYCLOAK_URL || 'No definido';
+  var keycloakRealm = process.env.KEYCLOAK_REALM || 'No definido';
+  var keycloakClientId = process.env.KEYCLOAK_CLIENT_ID || 'No definido';
+  var apiUrl = process.env.API_URL || 'No definido';
+
+  // Crear un string con las variables de entorno
+  var envVariables = `
+    KEYCLOAK_URL: ${keycloakUrl}
+    KEYCLOAK_REALM: ${keycloakRealm}
+    KEYCLOAK_CLIENT_ID: ${keycloakClientId}
+    API_URL: ${apiUrl}
+  `;
+
+  // Enviar las variables de entorno como respuesta
+  res.send(envVariables);
+});
+// Ruta de healthcheck
+app.get('/health', function (req, res) {
+  res.sendStatus(200); // Devuelve un estado 200 OK
 });
 
+// Inicia el servidor
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('App escuchando a http://%s:%s', host, port);
+  console.log('App escuchando en http://%s:%s', host, port);
 });
